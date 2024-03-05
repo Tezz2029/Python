3039cf87311cd61f9893900547fbd8cfdd432259
TEJAS <tejaslanjekar2000@gmail.com>
2024-03-05 10:26:40 +0530

    Python programs

---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+print(" Hello World \n ")
+print(" 'Text inside single quotes'\n ")
+
+a = input(" What is your name? ")
+print(a)
---
+++ swapNums.py
@@ -0,0 +1,9 @@
+a = input("Enter Num1 = ")
+b = input("Enter Num2 = ")
+
+temp = a
+a = b
+b = temp
+
+print("Value of Num1 = " + a)
+print("Value of Num2 = " + b)
