b3ea45e3617a80823694893e90afb56d2b330d12
TEJAS <tejaslanjekar2000@gmail.com>
2024-03-05 10:35:18 +0530

    Today's Programs

---
+++ additionOfTwoNum.py
@@ -0,0 +1,11 @@
+"""
+Program to add two Numbers
+"""
+
+print("Enter Number 1")
+a = int(input())
+print("Enter Number 2")
+b = int(input())
+
+ans = a + b
+print("Addition of two Numbers is", ans)
